5b3ffe49733fae2e3bf8c5652e21d748a9cfe2f8
tergrid <harshitsinghkalsi@gmail.com>
2024-12-14 22:54:18 +0530

    home page Finished

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,7 +1,11 @@
 import './homePage.css'
 import { Link } from "react-router-dom"
+import { TypeAnimation } from 'react-type-animation'
+import { useState } from 'react';
 
 const HomePage = () => {
+
+  const [typingStatus, setTypingStatus] = useState("Human-1");
   return (
     <div className='homePage'>
       <img src="./orbital.png" alt="" className='orbital' />
@@ -13,9 +17,37 @@ const HomePage = () => {
       </div>
       <div className="right">
         <div className="imgContainer">
-            <img src="/muffinbot.webp" alt="" className='bot'/>
+          <img src="/muffinbot.webp" alt="" className='bot' />
+          <div className="chat">
+            <img src={typingStatus === "Human-1" ? "/human1.jpeg" : typingStatus === 'Human-2' ? "/human2.jpeg" : "/muffinbot.webp"}alt="" className='chatImage'/>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Human:We produce food for Mice',
+                1000, ()=>{
+                  setTypingStatus('Bot')
+                },
+                'Bot:We produce food for Hamsters',
+                1000, ()=>{
+                  setTypingStatus('Human-2')
+                },
+                'Human:We produce food for Guinea Pigs',
+                1000, ()=>{
+                  setTypingStatus('Bot')
+                },
+                'Bot:We produce food for Chinchillas',
+                1000, ()=>{
+                  setTypingStatus('Human-1')
+                }
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
           </div>
         </div>
+      </div>
     </div>
   )
 }
